83a5d56964af2376c72c7b417d33ad20873e58ac
Young <youngheesomemoto@gmail.com>
2024-02-22 22:18:30 -0600

    switched to me API

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,52 +1,30 @@
-
-
 import { useEffect, useState } from "react";
 
 
 export function Account({ token, email, username }) {
     const [successMessage, setSuccessMessage] = useState("")
     const [book, setBook] = useState([]);
-
-
-    async function fetchUserData() {
-        try {
-            const response = await fetch("https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/users/me", {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`
-                },
-                body: JSON.stringify({ email: username, password: password })
-            });
-            const result = await response.json();
-            setSuccessMessage(result.message);
-        } catch (error) {
-            setError(error.message);
-        }
-
-    }
+    const [error, setError] =useState("")
 
     useEffect(() => {
-
-        async function fetchCheckIns(book) {
+        
+        async function fetchUserData() {
             try {
-                const response = await fetch("https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/reservations", {
+                const response = await fetch("https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/users/me", {
                     method: "GET",
                     headers: {
                         "Content-Type": "application/json",
                         Authorization: `Bearer ${token}`
                     },
-                    body: JSON.stringify(book),
                 });
                 const result = await response.json();
-
-                setBook(result.reservation ?? []);
-                return result.book;
+                setBook(result.books ?? []);
+                setSuccessMessage(result.message);
             } catch (error) {
-                console.error(error);
+                setError(error.message);
             }
         }
-        fetchCheckIns();
+        fetchUserData();
     }, []);
 
 
@@ -67,29 +45,34 @@ export function Account({ token, email, username }) {
     }
 
 
+
+
     return (
         <>
-        <h1>Hello, {username} </h1>
+            <h1>Hello, {username} </h1>
             <h2 className="account"> My library</h2>
-            <thead className="">
-                <tr>
-                    <th>name</th>
-                    <th>action</th>
-                </tr>
-            </thead>
-            <tbody className="">
-                {book.map((book) => {
-                    return (
-                        <tr key={book.id}>
-                            <td>{book.title}</td>
-                            <td>
-                                <button onClick={async () => await deleteCheckOuts(book.id)}>
-                                    Return </button>
-                            </td>
-                        </tr>
-                    )
-                })}
-            </tbody>
+            <table>
+                <thead className="">
+                    <tr>
+                        <th>name</th>
+                        <th>action</th>
+                    </tr>
+                </thead>
+                <tbody className="">
+                    {book.map((book) => {
+                        return (
+                            <tr key={book.id}>
+                                <td>{book.title}</td>
+                                <td>
+                                    <button onClick={async () => await deleteCheckOuts(book.id)}>
+                                        Return </button>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+
 
 
         </>
